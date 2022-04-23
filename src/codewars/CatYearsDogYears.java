13be1132ee7657567ed8d1ea4b684809dbe7d0ef
Aleksei Chapaev <alekseiachapaev@gmail.com>
2022-04-23 20:30:22 +0200

    Add task CatYearsDogYears

---
+++ src/codewars/CatYearsDogYears.java
@@ -0,0 +1,44 @@
+package codewars;
+
+import java.util.Arrays;
+
+public class CatYearsDogYears {
+
+    //https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b/train/java
+
+    public static int[] humanYearsCatYearsDogYears(final int humanYears) {
+
+        int[] arr = new int[3];
+        arr[0] = humanYears;
+
+        if(humanYears == 1){
+            arr[1] = 15;
+        }
+        if(humanYears == 2){
+            arr[1] = 15 + 9;
+        }
+        if(humanYears > 2){
+            int temp = humanYears - 2;
+            arr[1] = (temp * 4) + 15 + 9;
+        }
+
+        if(humanYears == 1){
+            arr[2] = 15;
+        }
+        if(humanYears == 2){
+            arr[2] = 15 + 9;
+        }
+        if(humanYears > 2){
+            int temp = humanYears - 2;
+            arr[2] = (temp * 5) + 15 + 9;
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(humanYearsCatYearsDogYears(2)));
+
+    }
+}
