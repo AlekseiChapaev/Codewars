a2d17b044312b1ff36133bfa5d8f86bc772568fc
Aleksei Chapaev <alekseiachapaev@gmail.com>
2022-04-25 23:10:49 +0200

    Add task refactored NeedleInTheHaystack

--- src/codewars/NeedleInTheHaystack.java
+++ src/codewars/NeedleInTheHaystack.java
@@ -1,37 +1,21 @@
 package codewars;
 
-import java.util.Arrays;
-
 public class NeedleInTheHaystack {
 
     //https://www.codewars.com/kata/56676e8fabd2d1ff3000000c
 
-//    public static String findNeedle(Object[] haystack) {
-//        String[] newArr = Arrays.toString(haystack).split(",");
-//        int index = 0;
-//        for (int i = 0; i < newArr.length; i++) {
-//            if(newArr[i] =="needle") {
-//                index = i;
-//            }
-//        }
-//
-//        return "found the needle at position " + index;
-//    }
+    public static String findNeedle(Object[] haystack) {
+        int index = 0;
+        for (int i = 0; i < haystack.length; i++) {
+            if (haystack[i] == "needle") {
+                index = i;
+            }
+        }
 
-//    public static String findNeedle(Object[] haystack) {
-//
-//        int index = 0;
-//        for (int i = 0; i < haystack.length; i++) {
-//            if(haystack[i].contains("needle")) {}=="needle") {
-//                index = i;
-//            }
-//        }
-//
-//        return "found the needle at position " + index;
-//    }
+        return "found the needle at position " + index;
+    }
 
-//    public static void main(String[] args) {
-//
-//        System.out.println(findNeedle(new Object[]{"3", "123124234", null, "needle", "world", "hay", 2, "3", true, false}));
-//    }
+    public static void main(String[] args) {
+        System.out.println(findNeedle(new Object[]{"3", "123124234", null, "needle", "world", "hay", 2, "3", true, false}));
+    }
 }
