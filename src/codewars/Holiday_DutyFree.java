6431ff5e50bc8e905e57fd0817ef15015c0cdc54
Aleksei Chapaev <alekseiachapaev@gmail.com>
2022-04-26 20:39:13 +0200

    Add task cHoliday_DutyFree

--- src/codewars/Holiday_DutyFree.java
+++ src/codewars/Holiday_DutyFree.java
@@ -5,11 +5,7 @@ public class Holiday_DutyFree {
     // https://www.codewars.com/kata/57e92e91b63b6cbac20001e5/train/java
 
     public static int dutyFree(int normPrice, int discount, int hol) {
-
-        double r = normPrice * discount / 100.0;
-        int res = (int)(hol / r);
-
-        return res;
+        return (int)(hol / normPrice * discount / 100.0);
     }
 
     public static void main(String[] args) {
