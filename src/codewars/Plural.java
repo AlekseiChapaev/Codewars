0cc8e33b3fd3b4244fe492ebd483d6666f8a4be5
Aleksei Chapaev <alekseiachapaev@gmail.com>
2022-05-02 20:26:01 +0200

    Add task Plural

---
+++ src/codewars/Plural.java
@@ -0,0 +1,9 @@
+package codewars;
+
+public class Plural {
+    //https://www.codewars.com/kata/52ceafd1f235ce81aa00073a/train/java
+
+    public static boolean isPlural(float f){
+        return f == 1;
+    }
+}
