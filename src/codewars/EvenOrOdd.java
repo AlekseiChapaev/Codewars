afddf387bc83e005bef84bc15122d89e38db051f
Aleksei Chapaev <alekseiachapaev@gmail.com>
2022-05-03 18:15:23 +0200

    Add task EvenOrOdd

---
+++ src/codewars/EvenOrOdd.java
@@ -0,0 +1,14 @@
+package codewars;
+
+public class EvenOrOdd {
+
+    //https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javav
+
+    public static String even_or_odd(int number) {
+        if(number % 2 == 0){
+            return "Even";
+        } else{
+            return "Odd";
+        }
+    }
+}
