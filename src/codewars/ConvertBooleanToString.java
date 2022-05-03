477c8a08ebd5f7e405bd4a468c5a1bd06b56d962
Aleksei Chapaev <alekseiachapaev@gmail.com>
2022-05-03 18:07:04 +0200

    Add task ConvertBooleanToString

---
+++ src/codewars/ConvertBooleanToString.java
@@ -0,0 +1,17 @@
+package codewars;
+
+public class ConvertBooleanToString {
+
+    //https://www.codewars.com/kata/551b4501ac0447318f0009cd
+
+    public static String convert(boolean b){
+        if(b) {
+            return "true";
+        } else {
+            return "false";
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
