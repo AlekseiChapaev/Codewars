13ccb800b4327368513b17b69d7f561d70ea2d67
Aleksei Chapaev <alekseiachapaev@gmail.com>
2023-02-06 21:16:55 +0100

    Add task Varargs

---
+++ src/codewars/Varargs.java
@@ -0,0 +1,15 @@
+package codewars;
+
+public class Varargs {
+    /**
+     * https://www.codewars.com/kata/55c90cad4b0fe31a7200001f/train/java
+     */
+
+    public static String buildString(String... args) {
+        return "I like " + String.join(", ", args) + "!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(buildString("Cheese", "Milk", "Chocolate"));
+    }
+}
