237f5eb6a0b9f04eb5b569fa9cb50ca594b1e0b3
Aleksei Chapaev <alekseiachapaev@gmail.com>
2023-02-06 20:08:17 +0100

    Add task SquaringAnArgument

---
+++ src/codewars/SquaringAnArgument.java
@@ -0,0 +1,11 @@
+package codewars;
+
+public class SquaringAnArgument {
+    public static int square(int n){
+        return (int)Math.pow(n, 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(square(2));
+    }
+}
