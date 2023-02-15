0ae6b1d9bf927a8135fc9875a6637a4efdea6f3c
Aleksei Chapaev <alekseiachapaev@gmail.com>
2023-02-15 21:25:38 +0100

    Add task MergingSortedArraysWithoutDuplicates

---
+++ src/codewars/MergingSortedArraysWithoutDuplicates.java
@@ -0,0 +1,19 @@
+package codewars;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class MergingSortedArraysWithoutDuplicates {
+    /**
+     * https://www.codewars.com/kata/573f5c61e7752709df0005d2/train/java
+     */
+    public static int[] mergeArrays(int[] first, int[] second) {
+
+        return IntStream.concat(Arrays.stream(first), Arrays.stream(second)).distinct().sorted().toArray();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(mergeArrays(new int[] { 2, 4, 8 }, new int[] { 2, 4, 6 })));
+
+    }
+}
