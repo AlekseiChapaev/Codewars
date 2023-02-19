40101178ba0617a8b17108d628fe3e88873271fb
Aleksei Chapaev <alekseiachapaev@gmail.com>
2023-02-19 17:29:10 +0100

    Add task ArrayMean

---
+++ src/codewars/ArrayMean.java
@@ -0,0 +1,16 @@
+package codewars;
+
+import java.util.Arrays;
+
+public class ArrayMean {
+    /**
+     * https://www.codewars.com/kata/55d277882e139d0b6000005d
+     */
+    public static int findAverage(int[] nums) {
+        return Arrays.stream(nums).sum() / nums.length;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findAverage(new int[]{1,3,5,7}));
+    }
+}
