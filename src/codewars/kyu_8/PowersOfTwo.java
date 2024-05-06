7b0fb976490d3a95e95eac1f446db8deae79f91f
Aleksei Chapaev <alekseiachapaev@gmail.com>
2024-05-06 20:27:50 +0200

    Add PowersOfTwo task with 2 solutions

---
+++ src/codewars/kyu_8/PowersOfTwo.java
@@ -0,0 +1,23 @@
+package codewars.kyu_8;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+public class PowersOfTwo {
+    //https://www.codewars.com/kata/57a083a57cb1f31db7000028/train/java
+    public static long[] powersOfTwo(int n){
+//        long[] res = new long[n + 1];
+//        for (int i = 0; i <= n; i++) {
+//            res[i] = (long)Math.pow(2, i);
+//        }
+//
+//        return res;
+
+        return LongStream.rangeClosed(0, n).map(i -> (int)Math.pow(2, i)).toArray();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(powersOfTwo(4)));
+    }
+}
