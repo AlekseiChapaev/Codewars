5d1052ac4f71f037cd4f31d6955b99c778206e49
Aleksei Chapaev <alekseiachapaev@gmail.com>
2024-05-07 11:52:02 +0200

    Add TotalPoints task

--- src/codewars/kyu_8/Training.java
+++ src/codewars/kyu_8/Training.java
@@ -1,23 +1,14 @@
 package codewars.kyu_8;
 
-import java.util.Arrays;
-
 public class Training {
 
-    public static String well(String[] x) {
-        long count = Arrays.stream(x).filter(s -> s.equals("good")).count();
+    public static String replace(final String s) {
+        return s.replaceAll("[aeuioAEIOU]", "!");
 
-        if (count == 0) return "Fail!";
-        if (count > 2) {
-            return "I smell a series!";
-        } else {
-            return "Publish!";
-        }
     }
 
     public static void main(String[] args) {
-
-        System.out.println(well(new String[]{"good", "bad", "bad", "bad", "bad", "good", "bad", "bad", "good"}));
+        System.out.println(replace("ABCDE"));
     }
 
 }
